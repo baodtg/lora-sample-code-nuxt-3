7a613329ffd1c5403d64a8a14a0e996850b4fbfa
baodtg <65148548+baodtg@users.noreply.github.com>
2024-01-19 03:47:10 +0000

    Integrate LORA

--- app.vue
+++ app.vue
@@ -1,5 +1,76 @@
+<script setup lang="ts">
+let player: any = undefined;
+onMounted(() => {
+  player = window.BeLivePlayerWidget.initialize({
+    showId: "a0832a15-b962-401e-9f95-5ea635d6373a",
+  });
+});
+
+onBeforeUnmount(() => {
+  player.removeAllListeners();
+});
+
+function close() {
+  player.close();
+}
+function minimize() {
+  player.minimize();
+}
+function unminimize() {
+  player.unminimize();
+}
+function watchLive() {
+  player.open();
+}
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div id="home">
+    <div class="alert alert-danger" role="alert">
+      We are restricting iframe from using our script. To make this sample code
+      work
+      <a href="/" target="_blank" class="alert-link">click here</a>
+      to open this page in another browser tab and try to click "Watch Live"
+      again
+    </div>
+    <div class="d-flex flex-column align-items-center justify-content-center">
+      <img class="mx-auto" alt="Vue logo" src="/nuxt-logo.svg" width="100px" />
+      <h1>Demo LORA Widget!</h1>
+      <div class="d-flex justify-content-center mt-5">
+        <button
+          @click="watchLive"
+          id="watch-button"
+          class="btn btn-primary mx-2"
+        >
+          Watch Live
+        </button>
+        <button @click="close" id="close-button" class="btn btn-primary mx-2">
+          Close Widget
+        </button>
+        <button
+          @click="minimize"
+          id="minimize-button"
+          class="btn btn-primary mx-2"
+        >
+          Minimize
+        </button>
+        <button
+          @click="unminimize"
+          id="unminimize-button"
+          class="btn btn-primary mx-2"
+        >
+          Unminimize
+        </button>
+      </div>
+    </div>
   </div>
 </template>
+
+<style>
+body {
+  min-height: 100vh;
+}
+#app {
+  text-align: center;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        {
+          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+          rel: "stylesheet",
+          integrity:
+            "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC",
+          crossorigin: "anonymous",
+        },
+      ],
+      script: [
+        {
+          type: "text/javascript",
+          src: "https://lora-sdk.belive.sg/player-widget/latest/player.min.js",
+          tagPosition: "bodyClose",
+        },
+      ],
+    },
+  },
+});
