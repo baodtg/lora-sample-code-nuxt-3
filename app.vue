45aa820d35e91627bcad4b915acc00c412b0ff8f
baodtg <baodtg@belive.sg>
2024-01-19 11:09:37 +0700

    Update showId

--- app.vue
+++ app.vue
@@ -1,76 +1,89 @@
 <script setup lang="ts">
 let player: any = undefined;
 onMounted(() => {
-  player = window.BeLivePlayerWidget.initialize({
-    showId: "a0832a15-b962-401e-9f95-5ea635d6373a",
-  });
+	player = window.BeLivePlayerWidget.initialize({
+		showId: "b5ed4b45-9d2f-4f92-aabd-992fb189c0ff",
+	});
+	player.on(window.BeLivePlayerWidget.PlayerEventType.READY, () => {
+		console.log("Player Widget Ready");
+	});
+	player.on(window.BeLivePlayerWidget.PlayerEventType.MINIMIZED, () => {
+		console.log("Player Widget MINIMIZED");
+	});
+
+	player.on(window.BeLivePlayerWidget.PlayerEventType.UNMINIMIZED, () => {
+		console.log("Player Widget UNMINIMIZED");
+	});
+	player.on(window.BeLivePlayerWidget.PlayerEventType.CLOSE, () => {
+		console.log("Player Widget CLOSE");
+	});
 });
 
 onBeforeUnmount(() => {
-  player.removeAllListeners();
+	player?.removeAllListeners();
 });
 
 function close() {
-  player.close();
+	player?.close();
 }
 function minimize() {
-  player.minimize();
+	player?.minimize();
 }
 function unminimize() {
-  player.unminimize();
+	player?.unminimize();
 }
 function watchLive() {
-  player.open();
+	player?.open();
 }
 </script>
 
 <template>
-  <div id="home">
-    <div class="alert alert-danger" role="alert">
-      We are restricting iframe from using our script. To make this sample code
-      work
-      <a href="/" target="_blank" class="alert-link">click here</a>
-      to open this page in another browser tab and try to click "Watch Live"
-      again
-    </div>
-    <div class="d-flex flex-column align-items-center justify-content-center">
-      <img class="mx-auto" alt="Vue logo" src="/nuxt-logo.svg" width="100px" />
-      <h1>Demo LORA Widget!</h1>
-      <div class="d-flex justify-content-center mt-5">
-        <button
-          @click="watchLive"
-          id="watch-button"
-          class="btn btn-primary mx-2"
-        >
-          Watch Live
-        </button>
-        <button @click="close" id="close-button" class="btn btn-primary mx-2">
-          Close Widget
-        </button>
-        <button
-          @click="minimize"
-          id="minimize-button"
-          class="btn btn-primary mx-2"
-        >
-          Minimize
-        </button>
-        <button
-          @click="unminimize"
-          id="unminimize-button"
-          class="btn btn-primary mx-2"
-        >
-          Unminimize
-        </button>
-      </div>
-    </div>
-  </div>
+	<div id="home">
+		<div class="alert alert-danger" role="alert">
+			We are restricting iframe from using our script. To make this sample code
+			work
+			<a href="/" target="_blank" class="alert-link">click here</a>
+			to open this page in another browser tab and try to click "Watch Live"
+			again
+		</div>
+		<div class="d-flex flex-column align-items-center justify-content-center">
+			<img class="mx-auto" alt="Vue logo" src="/nuxt-logo.svg" width="100px" />
+			<h1>Demo LORA Widget!</h1>
+			<div class="d-flex justify-content-center mt-5">
+				<button
+					@click="watchLive"
+					id="watch-button"
+					class="btn btn-primary mx-2"
+				>
+					Watch Live
+				</button>
+				<button @click="close" id="close-button" class="btn btn-primary mx-2">
+					Close Widget
+				</button>
+				<button
+					@click="minimize"
+					id="minimize-button"
+					class="btn btn-primary mx-2"
+				>
+					Minimize
+				</button>
+				<button
+					@click="unminimize"
+					id="unminimize-button"
+					class="btn btn-primary mx-2"
+				>
+					Unminimize
+				</button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style>
 body {
-  min-height: 100vh;
+	min-height: 100vh;
 }
 #app {
-  text-align: center;
+	text-align: center;
 }
 </style>
---
+++ index.d.ts
@@ -0,0 +1,9 @@
+declare global {
+	interface Window {
+		BeLivePlayerWidget: any;
+	}
+}
+
+window.BeLivePlayerWidget = window.BeLivePlayerWidget || {};
+
+export {};
